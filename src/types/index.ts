1929800f4c8ea13d1294b6ee073ac3e1d6fd5a05
Stepan <Stepashkabasketball@mail.ru>
2025-06-07 19:57:21 +0700

    updated readme

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,6 +7,11 @@ export interface IProductItem {
   price: number | null;
 }
 
+export interface IProductList {
+  total: number;
+  items: IProductItem[];
+}
+
 export interface IActions {
   onClick: (event: MouseEvent) => void;
 }
@@ -37,6 +42,10 @@ export interface IOrderResult {
   total: number;
 }
 
+export interface IOrderError {
+  error: string;
+}
+
 export interface ICartItem {
   product: IProductItem;
   quantity: number;
@@ -63,4 +72,34 @@ export interface IEventEmitter {
   on(event: string, callback: EventCallback): void;
   off(event: string, callback: EventCallback): void;
   emit(event: string, ...args: any[]): void;
+}
+
+export interface ApiResponse<T> {
+  success: boolean;
+  data?: T;
+  error?: string;
+}
+
+export interface IApiClient {
+  get<T>(endpoint: string): Promise<ApiResponse<T>>;
+  post<T>(endpoint: string, data: any): Promise<ApiResponse<T>>;
+  put<T>(endpoint: string, data: any): Promise<ApiResponse<T>>;
+  delete<T>(endpoint: string): Promise<ApiResponse<T>>;
+}
+
+export interface IProductModel {
+  getProducts(): Promise<IProductList>;
+  getProduct(id: string): Promise<IProductItem>;
+}
+
+export interface ICartModel {
+  getCart(): ICart;
+  addToCart(product: IProductItem): void;
+  removeFromCart(productId: string): void;
+  clearCart(): void;
+}
+
+export interface IOrderModel {
+  createOrder(form: IOrderLot): Promise<IOrderResult>;
+  getOrders(): Promise<IOrder[]>;
 }
